947bc447e867ae963fada1a3c71694fa2c3ad22e
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-14 21:17:25 -0700

    Update gcloud-sdk-bash.sh

--- gcloud-sdk-bash.sh
+++ gcloud-sdk-bash.sh
@@ -17,7 +17,7 @@ gcloud compute instances create rsyslog-server-test \
 --zone us-east1-b \
 #--tags "http-server","https-server" \
 --machine-type f1-micro \
---scopes cloud-platform
+--scopes cloud-platform \
 --metadata-from-file startup-script=ldap-rsyslog.sh
 gcloud compute instances create postgres-server-test \
 --image-family centos-7 \
@@ -25,7 +25,7 @@ gcloud compute instances create postgres-server-test \
 --zone us-east1-b \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
---scopes cloud-platform
+--scopes cloud-platform \
 --metadata-from-file startup-script=postgres.sh
 gcloud compute instances create ldap-server-test \
 --image-family centos-7 \
@@ -33,7 +33,7 @@ gcloud compute instances create ldap-server-test \
 --zone us-east1-b \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
---scopes cloud-platform
+--scopes cloud-platform \
 --metadata-from-file startup-script=ldap-server.sh
 gcloud compute instances create nfs_server_automation-test \
 --image-family centos-7 \
@@ -41,7 +41,7 @@ gcloud compute instances create nfs_server_automation-test \
 --zone us-east1-b \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
---scopes cloud-platform
+--scopes cloud-platform \
 --metadata-from-file startup-script=nfs_server_automation.sh
 
 
