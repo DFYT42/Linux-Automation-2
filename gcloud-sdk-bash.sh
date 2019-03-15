38bafc6833a165a695af6920e3175cd729308044
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-14 19:57:34 -0700

    Update and rename gcloud-sdk-bash to gcloud-sdk-bash.sh

---
+++ gcloud-sdk-bash.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##https://github.com/nic-instruction/hello-nti-310/blob/master/automate.md
+#GCloud
+##TEST
+##sudo su
+tail -f /var/messages
+
+#already has git installed
+#Setup for final
+git clone https://github.com/DFYT42/Linux-Automation-2/
+
+#Create seven instances
+##LOGSERVER##
+gcloud compute instances create rsyslog-server-test \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-east1-b \
+#--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform
+--metadata-from-file startup-script=ldap-rsyslog.sh
+gcloud compute instances create postgres-server-test \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-east1-b \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform
+--metadata-from-file startup-script=postgres.sh
+gcloud compute instances create ldap-server-test \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-east1-b \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform
+--metadata-from-file startup-script=ldap-server.sh
+gcloud compute instances create nfs_server_automation-test \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-east1-b \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform
+--metadata-from-file startup-script=nfs_server_automation.sh
+
+
+
+
