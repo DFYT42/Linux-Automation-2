03464f612b91971457def1a787be2ad8c6e2860f
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-14 19:53:01 -0700

    Update nfs_server_automation.sh

--- nfs_server_automation.sh
+++ nfs_server_automation.sh
@@ -27,3 +27,14 @@ echo "10.142.0.11:/var/nfsshare/testing     /mnt/test     nfs     defaults 0 0" 
 mount -a
 showmount -e $ipaddress
 showmount -e 10.142.0.11
+
+#setting up machine to run as client rsyslog to server rsyslog
+#install this on a server
+#rsyslog should be first server sun up
+#client automation
+sudo yum update -y && yum install -y rsyslog 	#CentOS 7
+sudo systemctl start rsyslog
+sudo systemctl enable rsyslog
+#on the client
+#add to end of file
+echo "*.* @@ldap-rsyslog-1:514" >> /etc/rsyslog.conf
