4e369b18df038eb47a315c7b169371bf38912c97
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-21 15:16:46 -0700

    Update gcloud-sdk-bash.sh

--- gcloud-sdk-bash.sh
+++ gcloud-sdk-bash.sh
@@ -29,7 +29,7 @@ gcloud compute instances create nti310-final-postgres \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=https://github.com/DFYT42/Linux-Automation-2/blob/master/postgres.sh
+--metadata-from-file startup-script=https://github.com/DFYT42/Linux-Automation-2/postgres.sh
 
 ##LDAPSERVER##
 #gcloud compute instances create nti310-final-ldapserver \
