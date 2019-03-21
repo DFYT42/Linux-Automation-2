3356b131e2ab0ee664f70ea5c836c749286f7836
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-21 14:15:18 -0700

    Update gcloud-sdk-bash.sh

--- gcloud-sdk-bash.sh
+++ gcloud-sdk-bash.sh
@@ -7,43 +7,63 @@
 
 #already has git installed
 #Setup for final
-git clone https://github.com/DFYT42/Linux-Automation-2/
+#git clone https://github.com/DFYT42/Linux-Automation-2/
 
 #Create seven instances
 ##LOGSERVER##
-gcloud compute instances create rsyslog-server-test \
+gcloud compute instances create nti310-final-logserver \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-east1-b \
 #--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=ldap-rsyslog.sh
-gcloud compute instances create postgres-server-test \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-east1-b \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=postgres.sh
-gcloud compute instances create ldap-server-test \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-east1-b \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=ldap-server.sh
-gcloud compute instances create nfs_server_automation-test \
---image-family centos-7 \
---image-project centos-cloud \
---zone us-east1-b \
---tags "http-server","https-server" \
---machine-type f1-micro \
---scopes cloud-platform \
---metadata-from-file startup-script=nfs_server_automation.sh
+--metadata-from-file startup-script=https://github.com/DFYT42/Linux-Automation-2/ldap-rsyslog.sh
 
+##POSTGRES##
+#gcloud compute instances create nti310-final-postgres \
+#--image-family centos-7 \
+#--image-project centos-cloud \
+#--zone us-east1-b \
+#--tags "http-server","https-server" \
+#--machine-type f1-micro \
+#--scopes cloud-platform \
+#--metadata-from-file startup-script=postgres.sh
 
+##LDAPSERVER##
+#gcloud compute instances create nti310-final-ldapserver \
+#--image-family centos-7 \
+#--image-project centos-cloud \
+#--zone us-east1-b \
+#--tags "http-server","https-server" \
+#--machine-type f1-micro \
+#--scopes cloud-platform \
+#--metadata-from-file startup-script=ldap-server.sh
+
+##NFSSERVER##
+gcloud compute instances create nti310-final-nfsserver \
+#--image-family centos-7 \
+#--image-project centos-cloud \
+#--zone us-east1-b \
+#--tags "http-server","https-server" \
+#--machine-type f1-micro \
+#--scopes cloud-platform \
+#--metadata-from-file startup-script=nfs_server_automation.sh
+
+##DJANGOSERVER##
+#gcloud compute instances create nti310-final-django-the-j-is-silent-server \
+#--image-family centos-7 \
+#--image-project centos-cloud \
+#--zone us-east1-b \
+#--tags "http-server","https-server" \
+#--machine-type f1-micro \
+#--scopes cloud-platform \
+#--metadata-from-file startup-script=nfs_server_automation.sh
+
+##CLIENTNFS##
+#Ubuntu 1804 LTS#
+
+##CLIENTLDAP##
+#Ubuntu 1804 LTS#
 
 
