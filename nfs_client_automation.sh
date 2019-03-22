d2b40e9273cbf7795332065e3cae2941d3122178
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-21 18:46:29 -0700

    Update nfs_client_automation.sh

--- nfs_client_automation.sh
+++ nfs_client_automation.sh
@@ -3,7 +3,7 @@
 ###NFS CLIENT###
 apt-get install nfs-client
 
-showmount -e nfs_server_automation-test
+###showmount -e nfs_server_automation-test
 mkdir /mnt/test
 echo "nti310-final-nfsserver:/var/nfsshare/testing       /mnt/test   nfs  defaults 0 0" >> /etc/fstab
 mount -a
