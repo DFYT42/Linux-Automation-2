09e8797c8eb5514423ea8566441a177174cec5cb
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2019-03-21 19:32:14 -0700

    Update nfs_server_automation.sh

--- nfs_server_automation.sh
+++ nfs_server_automation.sh
@@ -34,4 +34,4 @@ sudo systemctl start rsyslog
 sudo systemctl enable rsyslog
 #on the rsyslog client
 #add to end of file
-echo "*.* @@ldap-rsyslog-1:514" >> /etc/rsyslog.conf
+echo "*.* @@nti310-final-logserver:514" >> /etc/rsyslog.conf
